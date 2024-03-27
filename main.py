3ced6c091e739714d15f1fbff7b4073bae71643b
MichaelCintron <mikecintron@protonmail.com>
2024-03-27 11:26:19 -0400

    Put code into a function and cleaned up the comments in main.py
    Moved some of the comments from main.py to notes.txt

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
 import requests
+from decimal import Decimal
 from bs4 import BeautifulSoup
 
 
-# Access webpage's HTML locally to avoid spamming Lego.com
-with open("/home/michaelc/PycharmProjects/Lego_Prices/HTML_Docs/cat_owl_html_source.html") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
+def get_set_price(webpage_html, from_online=True):
+    '''
+    Give a Lego set's URL (or downloaded local file version of webpage), return the price of
+    that set
 
-# Or access the webpage's HTML and parse it directly
-# url = 'https://www.lego.com/en-us/product/71475'
-# # using headers to speed up response time
-# # https://stackoverflow.com/questions/62599036/python-requests-is-slow-and-takes-very-long-to-complete-http-or-https-request
-# headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
-# response = requests.get(url, headers=headers)
-# response_text = response.text
-# soup = BeautifulSoup(response_text, 'html.parser')
+    :param webpage_html: The URL of the Lego set's page, OR the local file location of a downloaded
+        copy of the Lego set's page (if local file, set <from_online=False>).
+    :param from_online: True by default, only change to False if webpage_html points to local file.
+    :return: Price of the Lego set in the decimal data type.
+    '''
+
+    # get the HTML from the URL or local file and send it into Beautiful Soup
+    if from_online:
+        # remotely
+        url = webpage_html
+        # using headers to speed up response time
+        # https://stackoverflow.com/questions/62599036/python-requests-is-slow-and-takes-very-long-to-complete-http-or-https-request
+        headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 ("
+                                 "KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+        response = requests.get(url, headers=headers)
+        response_text = response.text
+        soup = BeautifulSoup(response_text, 'html.parser')
+    else:
+        # locally
+        with open(webpage_html) as fp:
+            soup = BeautifulSoup(fp, 'html.parser')
+
+    # find the price data, get the price by itself, then change the price into a decimal.
+    prices = soup.find_all("meta", property='product:price:amount')
+    price = prices[0].get("content")  # set page should only have 1 price, but might need to make
+        # edge cases for when a set's page has more than 1 price on it.
+    price_decimal = Decimal(price)
+
+    return price_decimal
 
 
-# get the price of the Lego set from Lego.com
-# <meta property="product:price:amount" content="49.99"/>
-prices = soup.find_all("meta", property='product:price:amount')
-
-# print(prices)
-for price in prices:
-    print(price)
-    print(price.get("content"))
-
-
-# in the interest of not spamming lego's website, can I
-# just use the raw html file that I downloaded in its place?
-# YES YOU CAN: Make sure you download the webpage's HTML, NOT THE HTML
-# FOR THE 'view-source-page' PAGE!!
-
-# so now I have a program that can fetch the price of a given lego set, how do I
-# go about checking if it is on sale?
-
-# might be time to start git-iffing my project and breaking the program into
-# serperate functions and classes so I can have a 'working' version of the program
-# and a testing environment. Also would like to start Agile-ing the project too
-
-# John notes:
-# build a pipeline
-#     send from repo to online place (like AWS) automatically so that I don't have
-#     to manually compile code
-# push to an online place
-# trunk based dev
-# kanban board w/ user stories
-
+# html_source = 'https://www.lego.com/en-us/product/71476'
+# test_response = get_set_price(html_source)
+# html_source = '/home/michaelc/PycharmProjects/Lego_Prices/HTML_Docs/cat_owl_html_source.html'
+# test_response = get_set_price(html_source, from_online=False)
+# print(test_response, type(test_response))
