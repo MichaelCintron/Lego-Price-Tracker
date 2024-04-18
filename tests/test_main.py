116e9a1ec62f522c811dd17eb71b230cced6c1db
MichaelCintron <mikecintron@protonmail.com>
2024-04-18 12:29:48 -0400

    Removed old price fetch function and its tests, created test for get_price_lego_website
    All tests working again

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,27 +1,15 @@
 import unittest
 from decimal import Decimal
-from main import get_set_price
+from main import LegoPriceTracker
 
 
-class Test_get_set_price_local(unittest.TestCase):
-    """
-    Test get_set_price() with a download version of a Lego set's webpage
-    """
-    return_val_local = get_set_price('./HTML_Docs/cat_owl_html_source.html', from_online=False)
-
-    def test_return_value(self):
-        cat_owl_decimal_price = Decimal('49.99')
-        self.assertEqual(self.return_val_local, cat_owl_decimal_price)
-
-    def test_return_type(self):
-        self.assertIsInstance(self.return_val_local, Decimal)
-
-
-class Test_get_set_price_online(unittest.TestCase):
+class TestPriceLegoWebsite(unittest.TestCase):
     """
     Test get_set_price() with the live version of the Lego set's webpage
     """
-    return_val_online = get_set_price('https://www.lego.com/en-us/product/71476')
+
+    test_set = LegoPriceTracker('71476')
+    return_val_online = test_set.get_price_lego_website()
 
     def test_return_value(self):
         cat_owl_decimal_price = Decimal('49.99')
